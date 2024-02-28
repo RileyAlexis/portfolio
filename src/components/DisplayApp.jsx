832be751a8a8c18e3992770d48adfbf5e2791429
RileyAlexis <MxRileyAlexis@gmail.com>
2024-02-28 08:43:48 -0600

    modified DisplayApp component to be more functional on mobile screens

--- src/components/DisplayApp.jsx
+++ src/components/DisplayApp.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import { Stack, Typography, Paper, Button } from '@mui/material';
+import { Stack, Button, useMediaQuery, Typography } from '@mui/material';
 
 function DisplayApp({ image, link, source }) {
     const [isTextVisible, setIsTextVisible] = useState(false);
+    const isSmallScreen = useMediaQuery("(max-width: 600px)");
 
     const showText = () => {
         setIsTextVisible(true);
@@ -12,8 +13,45 @@ function DisplayApp({ image, link, source }) {
         setIsTextVisible(false);
     };
 
-    return (
-        // <Paper elevation={4}>
+    if (!isSmallScreen) {
+        return (
+            <div
+                style={{
+                    position: 'relative',
+                    width: '200px',
+                    height: '200px',
+                    background: `url("${image}") center/cover`,
+                    overflow: 'hidden',
+                }}
+                onMouseOver={showText}
+                onMouseOut={hideText}
+            >
+                <div
+                    style={{
+                        position: 'absolute',
+                        bottom: '0',
+                        left: '0',
+                        width: '100%',
+                        background: 'rgba(0, 0, 0, 0.7)',
+                        color: '#fff',
+                        padding: '20px',
+                        boxSizing: 'border-box',
+                        transform: isTextVisible ? 'translateY(0)' : 'translateY(100%)',
+                        transition: 'transform 0.3s ease-out',
+                    }}
+                >
+                    <Stack>
+                        <Button variant='empty' href={link} target="_blank">Open</Button>
+                        {/* <Button variant='empty' onClick={() => toggleAbout()}>About</Button> */}
+                        <Button variant='empty'
+                            href={source} target="_blank">
+                            Source
+                        </Button>
+                    </Stack>
+                </div>
+            </div>
+        );
+    } else return (
         <div
             style={{
                 position: 'relative',
@@ -22,8 +60,6 @@ function DisplayApp({ image, link, source }) {
                 background: `url("${image}") center/cover`,
                 overflow: 'hidden',
             }}
-            onMouseOver={showText}
-            onMouseOut={hideText}
         >
             <div
                 style={{
@@ -35,8 +71,6 @@ function DisplayApp({ image, link, source }) {
                     color: '#fff',
                     padding: '20px',
                     boxSizing: 'border-box',
-                    transform: isTextVisible ? 'translateY(0)' : 'translateY(100%)',
-                    transition: 'transform 0.3s ease-out',
                 }}
             >
                 <Stack>
@@ -49,8 +83,7 @@ function DisplayApp({ image, link, source }) {
                 </Stack>
             </div>
         </div>
-        // </Paper>
-    );
+    )
 };
 
 export default DisplayApp;
