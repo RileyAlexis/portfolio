5847a6d9a507c1945e960b389a24fdbf74f35a49
RileyAlexis <MxRileyAlexis@gmail.com>
2024-05-02 10:18:24 -0500

    updated bio

--- src/modules/paraData.js
+++ src/modules/paraData.js
@@ -4,7 +4,7 @@ const data = {
 }
 
 export const paraData = {
-    bio: [`Hello, I'm Riley Alexis, a new but capable web developer with a focus on full stack development. As a 2023 graduate of Prime Digital Academy I'm proficient in React, Redux, Node, Express and Postgres among numerous other web technologies. Please see the example applications below.`,
+    bio: [`Hello, I'm Riley Alexis, a capable web developer with a focus on full stack development. As a 2023 graduate of Prime Digital Academy I'm proficient in React, Redux, Node, Express and Postgres among numerous other web technologies. Please see the example applications below.`,
         `I am continually learning new technologies and creating new possibilities with my work and education. I was able to self host this site by running Ubuntu Server with an Nginx web host. This allows me to learn and deploy new technologies without being limited by a hosting services' choices.`,
         `I am currently available and looking for development work. Please see my resume on LinkedIn or peruse my Github.`],
 
